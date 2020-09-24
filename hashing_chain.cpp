c9ed985165fb83b4eb0ba65fffe8c27730b31a0e
muratersoy <muratersoy91@gmail.com>
2020-09-24 18:49:33 +0300

    Update hashing_chain.cpp

--- hashing_chain.cpp
+++ hashing_chain.cpp
@@ -1,95 +1,86 @@
 #include <iostream>
-#include <list> 
-#include <iterator> 
+#include <list>
+#include <iterator>
+
 using namespace std;
 
-class Hash
+class Hashing
 {
-    int hashbucket; // No. of hashbuckets
+    int no_hashbucket; // number of buckets
 
-    // pointer to an array containing hashbuckets
+    // pointer to an array containing buckets
     list<int> *table;
 
     public:
-    Hash(int V); //Constructor
+    Hashing(int V); // constructor
 
-    // inserts a key into hash table
-    void InsertItem(int x);
+    void item_insert(int x); // inserts a key into hash table
 
-    // deletes a key from hash table
-    void DeleteItem(int key);
+    void item_delete(int key); // deletes a key from hash table
 
-    // hash func map to values to key
-    int HashFunction(int x)
+    // hash function to map values to key
+    int hash_function(int x)
     {
-        return(x % hashbucket);
+        return (x % no_hashbucket);
     }
 
-    void DisplayHash();
-}; 
+    void display_hash();
+};
 
-Hash::Hash(int b)
+Hashing::Hashing(int b)
 {
-    this->hashbucket = b;
-    table = new list<int>[hashbucket];
+    this->no_hashbucket = b;
+    table = new list<int>[no_hashbucket];
 }
 
-void Hash::InsertItem(int key)
+void Hashing::item_insert(int key)
 {
-    int index = HashFunction(key);
+    int index = hash_function(key);
     table[index].emplace_back(key);
 }
 
-void Hash::DeleteItem(int key)
+void Hashing::item_delete(int key)
 {
-    // get the hash index(th) of key
-    int index = HashFunction(key);
+    int index = hash_function(key);
 
-    list<int> :: iterator i;
-    for(i = table[index].begin(); i != table[index].end(); ++i)
+    // find the key in (index)th list
+    list <int> :: iterator i;
+    for (i = table[index].begin(); i != table[index].end(); ++i)
     {
-        if(*i == key)
+        if (*i == key)
         break;
     }
 
     // if key is found in hash table, remove it
     if (i != table[index].end())
+    table[index].erase(i);
+}
+
+// function to display hash table
+void Hashing::display_hash()
+{
+    for(int i = 0; i < no_hashbucket; ++i)
     {
-        table[index].erase(i);
+        cout << i;
+        for (auto &x : table[i])
+        cout << " --> " << x;
+        cout << endl;
     }
 }
 
-    // function to display hash table
-    void Hash::DisplayHash()
-    {
-        for(int i = 0; i < hashbucket; ++i)
-        {
-            cout << i;
-            for (auto x : table[i])
-            {
-                cout << " ---> " << x;
-            }
-            cout << endl;
-        }
-    }
+int main()
+{
+    int a[] = {34, 114, 42, 56, 21, 68};
+    int n = sizeof(a)/sizeof(a[0]);
 
-    int main()
-    {
-        int a[] = {45, 13, 35, 78, 33};
-        int n = sizeof(a) / sizeof(a[0]);
-        
-        // insert a key into hash table, 7 is count of hashbuckets in hash table
-        Hash h(6); 
+    Hashing h(7);
 
-        for(int i = 0; i < n; ++i)
-        {
-            h.InsertItem(a[i]);
-        }
+    for (int i = 0; i < n; ++i)
+    h.item_insert(a[i]);
 
-        // delete 12 from hash table
-        h.DeleteItem(13);
+    //h.item_delete(114);
 
-        h.DisplayHash();
+    h.display_hash();
 
-        return 0;
-    }
+    return 0;
+}
